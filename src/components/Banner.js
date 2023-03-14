ad222b4939e02c4f6ebddd771b482210c4d45841
danielAwale <danielmadhukarawale@gmail.com>
2023-03-13 17:11:21 -0700

    contian all the imports for the banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,7 +1,14 @@
 import React from 'react';
 //images
-
 import Image from '../assets/banner.jpeg'
+//icons
+import { FaInstagram, FaTiktok, FaYoutube } from 'react-icons/fa'
+//type animation
+import { TypeAnimation } from 'react-type-animation';
+//motion
+import { motion } from 'framer-motion'
+// variants
+import { fadeIn } from '../variants'
 
 const Banner = () => {
   return <div className='section' id='home'>Banner</div>;
