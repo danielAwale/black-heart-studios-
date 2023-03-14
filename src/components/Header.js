215b2cc4f8169482527fb1572cf58b05641425a7
danielAwale <danielmadhukarawale@gmail.com>
2023-03-13 17:01:19 -0700

    contain the header section with the logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,20 @@
 import React from 'react';
+// images
+import Logo from '../assets/logo.jpeg'
 
 const Header = () => {
-  return <div className='bg-pink-200'>Header</div>;
+  return <div className='py-8'>
+    <div className='container mx-auto'>
+      <div className='flex justify-between items-center'>
+        {/* logo */}
+        <a href="#">
+          <img src={Logo} alt="logo" />
+        </a>
+        {/* button */}
+        <button className='btn btn-sm'>Work With Me</button>
+      </div>
+    </div>
+  </div>;
 };
 
 export default Header;
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -10,7 +10,7 @@ const Nav = () => {
   return <nav className='fixed bottom-2 lg:bottom-8 w-full overflow-hidden z-50'>
     <div className='container mx-auto'>
       <div className='w-full bg-black/20 h-[96px] backdrop-blur-2xl rounded-full max-w-[460px] mx-auto px-5 flex justify-between text-2xl items-center text-white/50'>
-        <Link to='home' smooth={true} spy={true} activeClass="active" className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center">
+        <Link to='home' smooth={true} spy={true} offset={-300} activeClass="active" className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center">
           <BiHomeAlt />
         </Link>
         <Link to='about' smooth={true} spy={true} activeClass="active" className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center">
