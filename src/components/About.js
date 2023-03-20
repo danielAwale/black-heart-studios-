106436226945999f0958268d4ade99acdf09168c
danielAwale <danielmadhukarawale@gmail.com>
2023-03-20 11:03:19 -0700

    contain all the imports

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,12 @@
 import React from 'react';
 //countup
+import CountUp from 'react-countup/build/CountUp';
+//intersection observer hook
+import { useInView } from 'react-intersection-observer';
+// motion
+import { motion } from 'framer-motion';
+//variant
+import { fadeIn } from '../variants';
 
 const About = () => {
   return <section className='section' id='about'>About</section>;
